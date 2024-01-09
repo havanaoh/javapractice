20fd173deb64cd288d1112df93e4ef6c6e095536
havanaoh <top2480@naver.com>
2024-01-09 17:34:39 +0900

    24.01.09

--- chap06/src/Project/Account.java
+++ chap06/src/Project/Account.java
@@ -4,10 +4,16 @@ public class Account {
 	String name; // 이름 필드(아이디로 사용)
 	String ssn;	 // 생년월일 (패스워드로 사용)
 	String tel;  // 
-<<<<<<< HEAD
 	int balance;
+	int age;
 	
-
+	public Account() {
+		System.out.println("내가만든 Account()");
+	}
+	
+	public Account(String str) {
+		
+	}
 	public Account(String name, String ssn, String tel) {
 		super();
 		this.name = name;
@@ -15,23 +21,7 @@ public class Account {
 		this.tel = tel;
 		
 	}
-
-
-	@Override
-	public String toString() {
-		return "Account [name=" + name + ", ssn=" + ssn + ", tel=" + tel + ", balance=" + balance + "]";
-=======
-	int age;
 	
-	public Account() {
-		System.out.println("내가만든 Account()");
-	
-	}
-	
-	public Account(String str) {
-		
-	}
-		
 	public Account(String name, String ssn, String tel, int age) {
 		super();
 		this.name/*필드에 있는 놈이라고 명시해주는것*/= name;
@@ -42,14 +32,8 @@ public class Account {
 
 	@Override
 	public String toString() {
-		return "Account [name=" + name + ", ssn=" + ssn + ", tel=" + tel + ", age=" + age + "]";
->>>>>>> 456167b6dd089685b459e6628b129bccd33ee33f
+		return "Account [name=" + name + ", ssn=" + ssn + ", tel=" + tel + ", balance=" + balance + "]";
 	}
 	
-	
-	
-<<<<<<< HEAD
-	
-=======
->>>>>>> 456167b6dd089685b459e6628b129bccd33ee33f
+
 }
--- chap06/src/Project/AccountEx.java
+++ chap06/src/Project/AccountEx.java
@@ -1,6 +1,5 @@
 package Project;
 
-<<<<<<< HEAD
 import java.util.Scanner;
 
 public class AccountEx {
@@ -85,22 +84,5 @@ public class AccountEx {
 		System.out.println("프로그램 종료");
 		}
 
-	
-		
-=======
-public class AccountEx {
-
-	public static void main(String[] args) {
-		Account acc = new Account(); 
-		acc.name = "김하나";
-		acc.ssn = "911224";
-		acc.tel = "010-1234-1256";
-		System.out.println(acc);
-		System.out.println(acc.name);
-		
-		Account acc1 = new Account("강하나", "980204", "010-1234-1234", 26);
-		System.out.println(acc1);
-
-	}
-
 }
+
