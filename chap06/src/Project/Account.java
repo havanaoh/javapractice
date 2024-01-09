7e0696dff5730233dbec79dd35f8b77e40f86484
havanaoh <155132554+havanaoh@users.noreply.github.com>
2024-01-09 17:28:54 +0900

    Update Account.java

--- chap06/src/Project/Account.java
+++ chap06/src/Project/Account.java
@@ -4,23 +4,19 @@ public class Account {
 	String name; // 이름 필드(아이디로 사용)
 	String ssn;	 // 생년월일 (패스워드로 사용)
 	String tel;  // 
-<<<<<<< HEAD
 	int balance;
-	
 
 	public Account(String name, String ssn, String tel) {
 		super();
 		this.name = name;
 		this.ssn = ssn;
 		this.tel = tel;
-		
 	}
 
 
 	@Override
 	public String toString() {
 		return "Account [name=" + name + ", ssn=" + ssn + ", tel=" + tel + ", balance=" + balance + "]";
-=======
 	int age;
 	
 	public Account() {
@@ -39,17 +35,10 @@ public class Account {
 		this.tel = tel;
 		this.age = age;
 	}
-
 	@Override
 	public String toString() {
 		return "Account [name=" + name + ", ssn=" + ssn + ", tel=" + tel + ", age=" + age + "]";
->>>>>>> 456167b6dd089685b459e6628b129bccd33ee33f
+
 	}
 	
-	
-	
-<<<<<<< HEAD
-	
-=======
->>>>>>> 456167b6dd089685b459e6628b129bccd33ee33f
 }
