1ae850e04faef8ca3b4eee4c71555d8ec80e4ba3
havanaoh <top2480@naver.com>
2024-02-13 16:48:04 +0900

    24.02.13

---
+++ _web0213/src/main/java/sec01/verify/exam02/MusicRunnable.java
@@ -0,0 +1,23 @@
+package sec01.verify.exam02;
+
+public class MusicRunnable implements Runnable{
+	
+//	public MusicRunnable() {
+//		this.MusicRunnable(){
+//			
+//		}
+//	}
+
+	
+	@Override
+	public void run() {
+		for (int i = 0; i < 3; i++) {
+			System.out.println("음악 재생");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+
+}
